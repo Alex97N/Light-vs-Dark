017d2177280ed08ce76808d4e2b1324becdbaa57
Alex <alex97nemtoc@gmail.com>
2019-11-12 10:47:43 -0800

    Card UI + Deck Started

---
+++ Dark-VS-Light/Assets/Scripts/PlayerDeck.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeck : MonoBehaviour
+{
+
+	public List<MonsterCard> deckMonsters = new List<MonsterCard>() ;
+
+	public int idMonster;
+	public int deckMonstersSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        deckMonstersSize = 10;
+
+		for(int i =0;i<deckMonstersSize;i++){
+		
+			idMonster = Random.Range(0,5);
+
+			deckMonsters[i] = MonsterCardDataBase.monsterCardList[idMonster];
+
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
