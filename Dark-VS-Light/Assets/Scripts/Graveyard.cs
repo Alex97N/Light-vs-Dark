a9eff5bf331ee83d5cac3a870112700bf90bd08d
Alex <alex97nemtoc@gmail.com>
2019-12-12 16:17:40 -0800

    table setup

---
+++ Dark-VS-Light/Assets/Scripts/Graveyard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graveyard : MonoBehaviour
+{
+    public  List<GameObject> monstersCardsInGrave = new List<GameObject> ();
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void addInGrave(GameObject go){
+        
+        monstersCardsInGrave.Add(go);
+    
+    }
+}
